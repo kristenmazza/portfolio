3556e932745452d933407cc72bbc0cc2e3dea934
kristenmazza <kbmazza@gmail.com>
2023-08-09 18:03:19 -0700

    Add projects section

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,13 @@
 import About from './About';
 import Intro from './Intro';
+import Projects from './Projects';
 
 export default function Content() {
   return (
     <>
       <Intro />
       <About />
+      <Projects />
     </>
   );
 }
