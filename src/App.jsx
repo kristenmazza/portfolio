6feb34933b0fe940e0e4d4fd988cff48c18d689e
kristenmazza <kbmazza@gmail.com>
2023-10-05 11:48:29 -0700

    Replace header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
+import { useEffect } from 'react';
 import './App.css';
 import Content from './components/Content';
 import Header from './components/Header';
+import { StyledEngineProvider } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: { fontFamily: 'Inter' },
+});
+
+function scrollToSection(id) {
+  const target = document.getElementById(id);
+  if (target) {
+    window.scrollTo({
+      top: target.offsetTop,
+      behavior: 'smooth',
+    });
+  }
+}
 
 function App() {
+  useEffect(() => {
+    const hash = window.location.hash.substring(1);
+    if (hash) {
+      scrollToSection(hash);
+    }
+  }, []);
+
   return (
-    <>
-      <Header />
-      <Content />
-    </>
+    <ThemeProvider theme={theme}>
+      <StyledEngineProvider injectFirst>
+        <Header />
+        <Content />
+      </StyledEngineProvider>
+    </ThemeProvider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,46 +1,220 @@
-import { useState, useEffect } from 'react';
-import styles from './Header.module.css';
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { styled } from '@mui/material/styles';
 
-export default function Header() {
-  const [navResize, setNavResize] = useState(false);
+const drawerWidth = 240;
 
-  const listenScrollEvent = () => {
-    window.scrollY > 10 ? setNavResize(true) : setNavResize(false);
+const NavLink = styled(Link)(() => ({
+  color: '#151515',
+  fontSize: '20px',
+  fontWeight: '300',
+  textTransform: 'Capitalize',
+  textDecoration: 'none',
+  '&:hover': {
+    textDecoration: 'underline',
+    textUnderlineOffset: '5px',
+    textDecorationThickness: '1px',
+    backgroundColor: 'transparent',
+    color: '#151515',
+  },
+  marginLeft: '1.5rem',
+}));
+
+const NavButton = styled(Button)(() => ({
+  color: '#151515',
+  textAlign: 'center',
+  textTransform: 'Capitalize',
+  textDecoration: 'none',
+  width: '100%',
+  '&:hover': {
+    backgroundColor: '#ebedf4',
+    color: '#151515',
+  },
+}));
+
+function DrawerAppBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
   };
 
-  useEffect(() => {
-    window.addEventListener('scroll', listenScrollEvent);
-    return () => {
-      window.removeEventListener('scroll', listenScrollEvent);
-    };
-  }, []);
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant='h6' sx={{ my: 2 }}>
+        <Link
+          href='/'
+          underline='none'
+          sx={{
+            color: '#151515',
+            fontSize: '25px',
+            fontWeight: '700',
+            '&:hover': {
+              color: '#151515',
+            },
+          }}
+        >
+          KM
+        </Link>
+      </Typography>
+      <Divider />
+      <List>
+        <ListItem disablePadding sx={{ justifyContent: 'center' }}>
+          <NavButton href='#about'>
+            <ListItemText
+              primaryTypographyProps={{ fontSize: '20px', fontWeight: '300' }}
+              primary='About'
+            />
+          </NavButton>
+        </ListItem>
+        <ListItem disablePadding sx={{ justifyContent: 'center' }}>
+          <NavButton href='#projects'>
+            <ListItemText
+              primaryTypographyProps={{ fontSize: '20px', fontWeight: '300' }}
+              primary='Projects'
+            />
+          </NavButton>
+        </ListItem>
+        <ListItem disablePadding sx={{ justifyContent: 'center' }}>
+          <NavButton href='https://blog.kristenmazza.dev/'>
+            <ListItemText
+              primaryTypographyProps={{ fontSize: '20px', fontWeight: '300' }}
+              primary='Blog'
+            />
+          </NavButton>
+        </ListItem>
+        <ListItem disablePadding sx={{ justifyContent: 'center' }}>
+          <NavButton href={'#contact'}>
+            <ListItemText
+              primaryTypographyProps={{ fontSize: '20px', fontWeight: '300' }}
+              primary='Contact'
+            />
+          </NavButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
     <>
-      <header>
-        <nav
-          className={`${styles.navbar} ${
-            navResize ? styles.activeHeaderResize : ''
-          }`}
+      <CssBaseline />
+      <AppBar
+        component='nav'
+        elevation={0}
+        sx={{
+          backgroundColor: '#fff',
+          borderBottom: '1px solid rgb(218,218,218)',
+          letterSpacing: '0',
+          WebkitFontSmoothing: 'antialiased',
+        }}
+      >
+        <Container maxWidth='lg'>
+          <Toolbar>
+            <IconButton
+              color='inherit'
+              aria-label='open drawer'
+              edge='start'
+              onClick={handleDrawerToggle}
+              sx={{
+                mr: 2,
+                display: { sm: 'none' },
+                color: '#151515',
+                '&:hover': {
+                  backgroundColor: '#ebedf4',
+                },
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant='h6'
+              component='div'
+              sx={{
+                flexGrow: 1,
+                textAlign: 'start',
+                display: { xs: 'none', sm: 'block' },
+              }}
+            >
+              <Link
+                href='/'
+                underline='none'
+                sx={{
+                  color: '#151515',
+                  fontSize: '25px',
+                  fontWeight: '700',
+                  '&:hover': {
+                    color: '#151515',
+                  },
+                  WebkitFontSmoothing: 'antialiased',
+                }}
+              >
+                KM
+              </Link>
+            </Typography>
+            <Box
+              sx={{
+                display: { xs: 'none', sm: 'block' },
+              }}
+            >
+              <NavLink href='#about'>About</NavLink>
+              <NavLink href='#projects'>Projects</NavLink>
+              <NavLink href='https://blog.kristenmazza.dev'>Blog</NavLink>
+              <NavLink href='#contact'>Contact</NavLink>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      <nav>
+        <Drawer
+          container={container}
+          variant='temporary'
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+            },
+            WebkitFontSmoothing: 'antialiased',
+          }}
         >
-          <div className={styles.navbarContainer}>
-            <div className={styles.logo}>
-              <a href='#'>KM</a>
-            </div>
-            <ul className={styles.navbarList}>
-              <li>
-                <a href='#about'>About</a>
-              </li>
-              <li className={styles.projects}>
-                <a href='#projects'>Projects</a>
-              </li>
-              <li className={styles.contact}>
-                <a href='#contact'>Contact</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </header>
+          {drawer}
+        </Drawer>
+      </nav>
     </>
   );
 }
+
+DrawerAppBar.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default DrawerAppBar;
